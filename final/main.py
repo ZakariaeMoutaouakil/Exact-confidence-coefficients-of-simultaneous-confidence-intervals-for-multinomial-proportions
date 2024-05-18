a7b369f25a6b1afd498c2d219f7ab624329faf47
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-05-18 19:39:25 +0200

    Wrapped test in a function

--- final/main.py
+++ final/main.py
@@ -9,30 +9,36 @@ from final.filter_candidates import filter_candidates
 from final.transform_to_probability_vector import transform_to_probability_vector
 from find_minimizer.find_minimizer import find_minimizer
 
-for N in [10, 20, 30]:
-    print("N =", N)
+
+def execute_analysis(
+        n: int,
+        k: int, confidence_interval_function: Callable[[float, int], Tuple[float, float]],
+        use_filtering: bool = True
+):
+    print("N =", n)
     start_time = time.time()  # Start time measurement
-    K = 3
-    confidence_interval_function: Callable[[float, int], Tuple[float, float]] = confidence_interval
-    ranked_endpoints = rank_endpoints(N, confidence_interval_function)
-    candidates = generate_unique_matrix(K - 1, ranked_endpoints)
-    filtered_candidates = filter_candidates(candidates)
-    min_value, minimizer = find_minimizer(N, candidates, multinomial_confidence_intervals)
+
+    ranked_endpoints = rank_endpoints(n, confidence_interval_function)
+    candidates = generate_unique_matrix(k - 1, ranked_endpoints)
+
+    if use_filtering:
+        candidates = filter_candidates(candidates)
+
+    min_value, minimizer = find_minimizer(n, candidates, multinomial_confidence_intervals)
     end_time = time.time()  # End time measurement
     elapsed_time = end_time - start_time  # Calculate elapsed time
-    print("Without Filtering:")
+
+    print(f"{'With' if use_filtering else 'Without'} Filtering:")
     print("Minimum value:", min_value)
     print("Minimizer:", transform_to_probability_vector(minimizer))
     print("Risk:", 1 - min_value)
     print("Elapsed time:", elapsed_time)
 
-    start_time = time.time()  # Start time measurement
-    second_min_value, minimizer = find_minimizer(N, filtered_candidates, multinomial_confidence_intervals)
-    end_time = time.time()  # End time measurement
-    elapsed_time = end_time - start_time  # Calculate elapsed time
-    print("With Filtering:")
-    print("Minimum value:", second_min_value)
-    print("Minimizer:", transform_to_probability_vector(minimizer))
-    print("Risk:", 1 - second_min_value)
-    print("Risk gain:", second_min_value - min_value)
-    print("Elapsed time:", elapsed_time)
+
+# Example usage
+if __name__ == '__main__':
+    N_values = [10, 20, 30]
+    K = 3
+    for N in N_values:
+        execute_analysis(N, K, confidence_interval, use_filtering=False)
+        execute_analysis(N, K, confidence_interval)
