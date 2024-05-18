61263785025f1e0f89274807f6652218c630536f
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-05-18 14:32:14 +0200

    Commit before making intervals depend on x_i

--- final/main.py
+++ final/main.py
@@ -6,13 +6,13 @@ from confidence_intervals.confidence_interval import confidence_interval
 from final.transform_to_probability_vector import transform_to_probability_vector
 from find_minimizer.find_minimizer import find_minimizer
 
-N = 15
-K = 4
-confidence_interval_function: Callable[[float, int], Tuple[float, float]] = confidence_interval
-ranked_endpoints = rank_endpoints(N, confidence_interval_function)
-print("Ranked endpoints between 0 and 1:", ranked_endpoints)
-candidates = generate_unique_matrix(K - 1, ranked_endpoints)
-print("Candidates:", candidates)
-min_value, minimizer = find_minimizer(N, candidates, confidence_interval_function)
-print("Minimum value:", min_value)
-print("Minimizer:", transform_to_probability_vector(minimizer))
+for N in [10, 20, 30]:
+    K = 3
+    confidence_interval_function: Callable[[float, int], Tuple[float, float]] = confidence_interval
+    ranked_endpoints = rank_endpoints(N, confidence_interval_function)
+    print("Ranked endpoints between 0 and 1:", ranked_endpoints)
+    candidates = generate_unique_matrix(K - 1, ranked_endpoints)
+    print("Candidates:", candidates)
+    min_value, minimizer = find_minimizer(N, candidates, confidence_interval_function)
+    print("Minimum value:", min_value)
+    print("Minimizer:", transform_to_probability_vector(minimizer))
--- find_minimizer/find_minimizer.py
+++ find_minimizer/find_minimizer.py
@@ -7,7 +7,8 @@ from coverage_probability.coverage_probability import coverage_probability
 def find_minimizer(
         n: int,
         candidates: List[List[float]],
-        confidence_interval_function: Callable[[float, int], Tuple[float, float]]
+        confidence_interval_function: Callable[[float, int], Tuple[float, float]],
+        debug: bool = False
 ) -> Tuple[float, List[float]]:
     """Finds the minimizer of the elements in the list."""
     min_value, minimizer = float('inf'), []
@@ -15,6 +16,8 @@ def find_minimizer(
     for p in candidates:
         intervals = [confidence_interval_function(p_i, n) for p_i in p]
         cov_proba = coverage_probability(n, p, intervals)
+        if debug:
+            print(p, cov_proba)
 
         if cov_proba < min_value:
             min_value, minimizer = cov_proba, p
@@ -31,6 +34,6 @@ if __name__ == '__main__':
         [0.3, 0.4, 0.5]
     ]
 
-    min_value, minimizer = find_minimizer(n_example, candidates_example, confidence_interval)
+    min_value, minimizer = find_minimizer(n_example, candidates_example, confidence_interval, debug=True)
     print(f"Minimum coverage probability: {min_value}")
     print(f"Minimizing candidate: {minimizer}")
